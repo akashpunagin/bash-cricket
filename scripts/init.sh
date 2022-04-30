f0c037cef0cde08de4587b52469a9e81ddb80b91
akash <akash.punagin@gmail.com>
2022-04-30 17:06:59 +0530

    created utilities, init completed

---
+++ scripts/init.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+UTILITIES_PATH="./utilities"
+
+function addTeam() {
+    local TEAM_PATH=$1
+    mkdir $TEAM_PATH
+}
+
+function addPlayers() {
+    local N_PLAYERS=$1
+    local TEAM_NAME=$2
+    local TEAM_PATH=$3
+    
+    for ((i=1; i<=$N_PLAYERS; i++)); do
+        NAME=$($UTILITIES_PATH/prompt \
+            "Player Details of $TEAM_NAME (Team A)" \
+            "Enter name of player-$i" \
+            "Please enter valid name for player-$i")
+
+        touch ./$TEAM_PATH/$NAME
+    done
+
+    ./$UTILITIES_PATH/showInfo "Players Added" "Players added to team: $2"
+}
+
+### DRIVER CODE ###
+
+rm -rf ../teams
+mkdir ../teams
+
+read -p "Enter team A name: " TEAM_A
+
+read -p "Enter team B name: " TEAM_B
+
+TEAM_A_PATH="../teams/$TEAM_A"
+TEAM_B_PATH="../teams/$TEAM_B"
+
+addTeam $TEAM_A_PATH
+addTeam $TEAM_B_PATH
+
+read -p "Enter members in each team: " N_PLAYERS
+
+addPlayers $N_PLAYERS $TEAM_A $TEAM_A_PATH
+addPlayers $N_PLAYERS $TEAM_B $TEAM_B_PATH
--- scripts/start_game.sh
+++ scripts/start_game.sh
@@ -1,31 +1,20 @@
-function prompt() {
-    # args
-    # 1. title of dialog
-    # 2. text of text field
-    # 3. text of warning if input from user is invalid
+#!/bin/bash
 
-    NAME=$(zenity --entry --title="$1" --text="$2")
+UTILITIES_PATH="./utilities"
 
-    # if the length of string is zero then read value again
-    while [ -z "$NAME" ]; do
-        zenity --warning --text="$3" --no-wrap
-        NAME=$(zenity --entry --title="$1" --text="$2")
-    done
+### DRIVER CODE ###
 
-    echo $NAME
-    return 0
-}
+if [ -d "../teams" ]; then
+    RES=$(./$UTILITIES_PATH/showConfirmation "Players exist" "Do you want to remove existing teams and add new teams?")
+    if [ "$RES" -eq 0 ]; then
+        # dont add players, play with existing players
+        echo "Playing with existing players"
+    else
+        # add team, players, and play
+        bash init.sh
+    fi
+else
+    bash init.sh
+fi
 
 
-# read -p "Enter team A name: " TEAM_A
-
-# read -p "Enter team B name: " TEAM_B
-
-read -p "Enter members in each team: " N_PLAYERS
-
-echo "TOTAL PLAYERS: $(echo $N_PLAYERS | bc)"
-
-for ((i=1; i<=$N_PLAYERS; i++)); do
-    prompt "Player Details" "Enter name of player-$i" "Please enter valid name for player-$i"
-    # echo "$i"
-done
